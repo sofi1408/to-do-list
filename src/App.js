e4c7b129d4dbb752e299188d27d4333f28305528
Sofia Goyal <sofia.goyal@care.com>
2023-09-25 08:50:00 +0200

    final code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
+import { useState } from "react";
+
 import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+
+import List from "./components/List/List";
 
 function App() {
+  const [text, setText] = useState("");
+  const [todolist, setTodolist] = useState([]);
+
+  const addItem = () => {
+    const newTodoItem = {
+      id: uuidv4(),
+      item: text,
+      done: false,
+    };
+    setTodolist([...todolist, newTodoItem]);
+    setText("");
+  };
+
+  const handleToggle = (itemId) => {
+    const newTodolist = todolist.map((listItem) => {
+      if (listItem.id === itemId) {
+        return { ...listItem, done: !listItem.done };
+      }
+      return listItem;
+    });
+    setTodolist(newTodolist);
+  };
+
+  const handleDelete = (itemId) => {
+    const newTodolist = todolist.filter((listItem) => listItem.id !== itemId);
+    setTodolist(newTodolist);
+  };
+
   return (
     <div className="App">
       <h1>To Do List</h1>
+      <div className="adder">
+        <input
+          type="text"
+          placeholder="Add Items to your list"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <span onClick={addItem}>+</span>
+      </div>
+      {todolist.length > 0 && (
+        <List
+          todolist={todolist}
+          handleToggle={handleToggle}
+          handleDelete={handleDelete}
+        />
+      )}
     </div>
   );
 }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,27 @@
 import "./List.css";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
 
-export const List = () => {
-  return <ul className="list"></ul>;
+export const List = ({ todolist, handleToggle, handleDelete }) => {
+  return (
+    <ul className="list">
+      {todolist.map((listItem) => (
+        <li key={listItem.id} className={listItem.done ? "done" : ""}>
+          <span>{listItem.item}</span>
+          <span className="actions">
+            {listItem.done ? (
+              <CancelIcon onClick={() => handleToggle(listItem.id)} />
+            ) : (
+              <CheckCircleIcon onClick={() => handleToggle(listItem.id)} />
+            )}
+
+            <DeleteForeverIcon onClick={() => handleDelete(listItem.id)} />
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default List;
